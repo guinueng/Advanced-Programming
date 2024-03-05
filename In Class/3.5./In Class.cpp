cbb3fd6dd87dc958549ef7742203b14830a44aff
Guinueng <admin@guinueng.com>
2024-03-05 15:57:45 +0900

    Reverting Process

---
+++ In Class/3.5./In Class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+long fact(long); //> Function prototype. Inform compiler this function exist somewhere.
+//#define X 1 + 1
+//#define until(cond) while(!(cond))
+//Preprocessor understand pherenblabla.
+
+int main(){
+    /*int ans = 1;
+    int i = 10;
+    until (i == 0){ // > loop
+        ans *= i;
+        i--;
+    }*/
+    std::cout<<fact(9)<<std::endl;
+    return 0;
+    //return X * X;
+}//Not using #incl.... to be simple file.
+// g++ -E "file name" > preprocess c++.
+// Preprocessor doesn't understand c++.
+// Only knows cpy and paste.
+// If we define x 1 + 1 and return x*x > preprocess will return 1 + 1 * 1 + 1 = 3.
+// g++ -S "File name" > creates .s file which is assembly file.
+// linker error contains 'ld' reference.
